f674f0bc0ce81603a941b02a844eb13939b601db
Vinai Kopp <vinai@netzarbeiter.com>
2020-11-20 14:50:06 +0100

    Add support for cell block renderers defined in layout XML

--- Test/Functional/ViewModel/HyvaGrid/CellTest.php
+++ Test/Functional/ViewModel/HyvaGrid/CellTest.php
@@ -5,6 +5,9 @@ namespace Hyva\Admin\Test\Functional\ViewModel\HyvaGrid;
 use Hyva\Admin\ViewModel\HyvaGrid\Cell;
 use Hyva\Admin\ViewModel\HyvaGrid\CellInterface;
 use Hyva\Admin\ViewModel\HyvaGrid\ColumnDefinitionInterface;
+use Magento\Framework\View\Element\AbstractBlock;
+use Magento\Framework\View\Element\Context as BlockContext;
+use Magento\Framework\View\LayoutInterface;
 use Magento\TestFramework\ObjectManager;
 use PHPUnit\Framework\TestCase;
 
@@ -59,5 +62,32 @@ class CellTest extends TestCase
         /** @var Cell $cell */
         $cell = ObjectManager::getInstance()->create(CellInterface::class, $arguments);
         $this->assertSame('<div>Test: EUR::10.10</div>', trim($cell->getHtml(), PHP_EOL));
+    }
+
+    public function testRendersUsingExistingBlock(): void
+    {
+        /** @var LayoutInterface $layout */
+        $layout = ObjectManager::getInstance()->get(LayoutInterface::class);
+        $blockContext = ObjectManager::getInstance()->get(BlockContext::class);
+        $rendererBlock = new class($blockContext) extends AbstractBlock {
+            protected function _toHtml()
+            {
+                /** @var CellInterface $cell */
+                $cell = $this->getData('cell');
+                return '!!' . $cell->getTextValue() . '!!';
+            }
+        };
+
+        $rendererBlockName = 'test-renderer';
+        $layout->setBlock($rendererBlockName, $rendererBlock);
+        $rendererBlock->setNameInLayout($rendererBlockName);
+
+        $value            = ['currency' => 'EUR', 'amount' => 10.10];
+        $columnDefinition = $this->createColumnDefinition('array', ['rendererBlockName' => $rendererBlockName]);
+        $arguments        = ['value' => $value, 'columnDefinition' => $columnDefinition];
+
+        /** @var Cell $cell */
+        $cell = ObjectManager::getInstance()->create(CellInterface::class, $arguments);
+        $this->assertSame('!![EUR, 10.1]!!', $cell->getHtml());
     }
 }
