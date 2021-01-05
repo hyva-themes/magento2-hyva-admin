8e4ee5eca6705a5c7ea73b5929c6aafe3f181796
Vinai Kopp <vinai@netzarbeiter.com>
2021-01-05 12:10:23 +0100

    Fiddle with paths to get the backportability test working

--- build/backport-and-unit-test.sh
+++ build/backport-and-unit-test.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 #
-# This script is intended to be used in a MAGENTO_POST_INSTALL_SCRIPT script in the
+# This script is intended to be sourced as a MAGENTO_POST_INSTALL_SCRIPT script in the
 # https://github.com/extdn/github-actions-m2 integration test github action.
 #
-# If this script exits with an error status, the calling script will fail, too, because the -e bash option is set.
+# Note: if this script exits with an error status, the calling script will fail, too.
 #
 # Reference: https://github.com/extdn/github-actions-m2/blob/master/magento-integration-tests/entrypoint.sh#L90-L94
 #
 
-set -e
 
 BUILD_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-echo "Executing backport to PHP 7.3 and unit test build scripts in directory" ${BUILD_SCRIPT_DIR}
+echo "Sourcing backport to PHP 7.3 and unit test build scripts in directory" ${BUILD_SCRIPT_DIR}
 
-${BUILD_SCRIPT_DIR}/backport-php73.sh
-${BUILD_SCRIPT_DIR}/run-unit-tests.sh
+. ${BUILD_SCRIPT_DIR}/backport-php73.sh
+. ${BUILD_SCRIPT_DIR}/run-unit-tests.sh
--- build/run-unit-tests.sh
+++ build/run-unit-tests.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 #
-# This script is intended to be used in a MAGENTO_POST_INSTALL_SCRIPT script in the
+# This script is intended to be sourced by a MAGENTO_POST_INSTALL_SCRIPT script in the
 # https://github.com/extdn/github-actions-m2 integration test github action.
 #
-# If this script exits with an error status, the calling script will fail, too, because the -e bash option is set.
+# Note: If this script exits with an error status, the calling script will fail, too.
 #
 # Reference: https://github.com/extdn/github-actions-m2/blob/master/magento-integration-tests/entrypoint.sh#L90-L94
 #
 
-set -e
+echo "Changing the current working directory to ${MAGENTO_ROOT}"
+cd $MAGENTO_ROOT
 
 echo "Running unit tests with PHP " $(php --version | head -1)
 
 vendor/bin/phpunit -c dev/tests/unit/phpunit.xml.dist vendor/hyva-themes/module-magento2-admin/Test/Unit
+
+echo "Changing the current working directory back to it's previous location."
+cd -
