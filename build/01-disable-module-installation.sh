5d3ac3bd5622865e795ee09664396b1546be748f
Vinai Kopp <vinai@netzarbeiter.com>
2021-01-06 12:07:36 +0100

    Disable module in local-source before composer install

--- build/01-disable-module-installation.sh
+++ build/01-disable-module-installation.sh
@@ -4,5 +4,5 @@
 # https://github.com/extdn/github-actions-m2/blob/master/magento-integration-tests/entrypoint.sh
 
 echo "Disabling module ${COMPOSER_NAME} during initial installation"
-mv -v ${MAGENTO_ROOT}/vendor/${COMPOSER_NAME}/registration.php \
-      ${MAGENTO_ROOT}/vendor/${COMPOSER_NAME}/registration-disabled.php
+mv -v ${MAGENTO_ROOT}/local-source/${GITHUB_ACTION}/registration.php \
+      ${MAGENTO_ROOT}/local-source/${GITHUB_ACTION}/registration-disabled.php
