8f494dc8346999aff3b23bcb34834c4e60d685d9
Vinai Kopp <vinai@netzarbeiter.com>
2021-01-14 11:18:32 +0100

    Switch to repo-magento-mirror.fooman.co.nz so no credentials are required

--- build/unit-test-entrypoint.sh
+++ build/unit-test-entrypoint.sh
@@ -17,11 +17,8 @@ test -z "${MAGENTO_VERSION}" && (echo "'magento_version' is not set" && exit 1)
 MAGENTO_ROOT=/tmp/m2
 PROJECT_PATH=$GITHUB_WORKSPACE
 
-#echo "Setup Magento credentials"
-#composer global config http-basic.repo.magento.com $MAGENTO_MARKETPLACE_USERNAME $MAGENTO_MARKETPLACE_PASSWORD
-
 echo "Prepare composer installation"
-composer create-project --repository=https://repo.magento.com/ magento/project-community-edition:${MAGENTO_VERSION} $MAGENTO_ROOT --no-install --no-interaction --no-progress
+composer create-project --repository=https://repo-magento-mirror.fooman.co.nz/ --no-install --no-progress --no-plugins magento/project-community-edition $MAGENTO_ROOT "$MAGENTO_VERSION"
 
 echo "Setup extension source folder within Magento root"
 cd $MAGENTO_ROOT
@@ -31,6 +28,8 @@ cp -R ${GITHUB_WORKSPACE}/${MODULE_SOURCE} $MODULE_NAME
 
 echo "Configure extension source in composer"
 cd $MAGENTO_ROOT
+composer config --unset repo.0
+composer config repositories.foomanmirror composer https://repo-magento-mirror.fooman.co.nz/
 composer config repositories.local-source path local-source/\*
 composer require $COMPOSER_NAME:@dev --no-update --no-interaction
 
