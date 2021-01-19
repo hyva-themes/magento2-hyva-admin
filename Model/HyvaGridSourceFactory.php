93c21e98494db9907a965d411547cb1dfad1213b
Vinai Kopp <vinai@netzarbeiter.com>
2021-01-19 13:54:35 +0100

    Pass defaultSearchCriteriaBinding config to SearchCriteriaBindings

--- Model/HyvaGridSourceFactory.php
+++ Model/HyvaGridSourceFactory.php
@@ -2,6 +2,7 @@
 
 namespace Hyva\Admin\Model;
 
+use Hyva\Admin\Model\GridSource\SearchCriteriaBindings;
 use function array_merge as merge;
 use Hyva\Admin\Model\GridSourceType\SourceTypeLocator;
 
@@ -28,17 +29,24 @@ class HyvaGridSourceFactory
             throw new \RuntimeException($message);
         }
 
-        $constructorArguments = [
+        $sharedConstructorArguments = [
             'gridName'            => $gridDefinition->getName(),
             'sourceConfiguration' => $gridSourceConfiguration,
         ];
-        $gridSourceType       = $this->objectManager->create(
+        $gridSourceType             = $this->objectManager->create(
             $this->sourceTypeLocator->getFor($gridDefinition->getName(), $gridSourceConfiguration),
-            $constructorArguments
+            $sharedConstructorArguments
         );
+        $bindingsConfig             = $gridSourceConfiguration['defaultSearchCriteriaBindings'] ?? [];
+        $searchCriteriaBindings     = $this->objectManager->create(
+            SearchCriteriaBindings::class,
+            merge(['bindingsConfig' => $bindingsConfig], $sharedConstructorArguments)
+        );
+
+        $dependencies = ['gridSourceType' => $gridSourceType, 'searchCriteriaBindings' => $searchCriteriaBindings];
         return $this->objectManager->create(
             HyvaGridSourceInterface::class,
-            merge(['gridSourceType' => $gridSourceType], $constructorArguments)
+            merge($dependencies, $sharedConstructorArguments)
         );
     }
 }
