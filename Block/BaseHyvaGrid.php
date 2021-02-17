12ad464a41b0f2a301beb610c6d28af074b8bd1e
Andreas Pointner <andreas.pointner@copex.io>
2021-02-17 08:51:04 +0100

    Removed children definition of base hyva grid

--- Block/BaseHyvaGrid.php
+++ Block/BaseHyvaGrid.php
@@ -4,14 +4,11 @@ namespace Hyva\Admin\Block;
 
 use Hyva\Admin\ViewModel\HyvaGridInterface;
 use Hyva\Admin\ViewModel\HyvaGridInterfaceFactory;
-use Magento\Framework\View\Element\Block\ArgumentInterface;
 use Magento\Framework\View\Element\Template;
 
 abstract class BaseHyvaGrid extends Template
 {
     private HyvaGridInterfaceFactory $gridFactory;
-
-    private array $children;
 
     private HyvaGridInterface $grid;
 
@@ -19,13 +16,11 @@ abstract class BaseHyvaGrid extends Template
         Template\Context $context,
         string $gridTemplate,
         HyvaGridInterfaceFactory $gridFactory,
-        array $children = [],
         array $data = []
     ) {
         $this->setTemplate($gridTemplate);
         parent::__construct($context, $data);
         $this->gridFactory = $gridFactory;
-        $this->children = $children;
     }
 
     public function getGrid(): HyvaGridInterface
@@ -44,42 +39,29 @@ abstract class BaseHyvaGrid extends Template
 
     public function getNavigationHtml(): string
     {
-        $renderer = $this->createRenderer("navigation");
+        $renderer = $this->createRenderer();
+        $renderer->setTemplate('Hyva_Admin::element/navigation.phtml');
         $renderer->assign('navigation', $this->getGrid()->getNavigation());
         return $renderer->toHtml();
     }
 
     public function getActionsHtml(): string
     {
-        $renderer = $this->createRenderer("actions");
+        $renderer = $this->createRenderer();
+        $renderer->setTemplate('Hyva_Admin::element/actions.phtml');
         $renderer->assign('actions', $this->getGrid()->getActions());
         return $renderer->toHtml();
     }
 
     public function getExportsHtml(): string
     {
-        $renderer = $this->createRenderer("exports");
+        $renderer = $this->createRenderer();
+        $renderer->setTemplate('Hyva_Admin::element/exports.phtml');
         $renderer->assign('exports', $this->getGrid()->getNavigation()->getExports());
         return $renderer->toHtml();
     }
 
-    private function createRenderer($name): Template {
-        if ($blockDefinition = $this->children[$name] ?? false) {
-            /** @var Template $renderer */
-            $renderer = $this->_layout->createBlock(Template::class);
-            if (!isset($blockDefinition['template'])) {
-                throw new \InvalidArgumentException('Child template missing');
-            }
-            $renderer->setTemplate($blockDefinition['template']);
-
-            if (isset($blockDefinition['view_model'])) {
-                if (!$blockDefinition['view_model'] instanceof ArgumentInterface) {
-                    throw new \InvalidArgumentException('ViewModel should be instance of ArgumentInterface');
-                }
-                $renderer->assign('view_model', $blockDefinition['view_model']);
-            }
-            return $renderer;
-        }
-        throw new \InvalidArgumentException(sprintf("Block %s not found", $name));
+    private function createRenderer(): Template {
+        return $this->_layout->createBlock(Template::class);
     }
 }
