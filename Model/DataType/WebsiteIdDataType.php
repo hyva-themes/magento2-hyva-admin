abdb897897881a14b7b2d40ab90b0368470d16ab
Vinai Kopp <vinai@netzarbeiter.com>
2021-04-21 15:26:39 +0200

    Actually implement website_id column type

--- Model/DataType/WebsiteIdDataType.php
+++ Model/DataType/WebsiteIdDataType.php
@@ -8,61 +8,38 @@ use Magento\Store\Model\System\Store as SystemStore;
 
 use function array_map as map;
 
-class StoreIdDataType implements DataTypeValueToStringConverterInterface
+class WebsiteIdDataType implements DataTypeValueToStringConverterInterface
 {
-    public const TYPE_STORE_ID = 'store_id';
+    public const TYPE_STORE_ID = 'website_id';
 
     /**
      * @var SystemStore
      */
     private $systemStore;
 
-    /**
-     * @var Escaper
-     */
-    private $escaper;
-
-    public function __construct(SystemStore $systemStore, Escaper $escaper)
+    public function __construct(SystemStore $systemStore)
     {
         $this->systemStore = $systemStore;
-        $this->escaper     = $escaper;
     }
 
     public function toString($value): ?string
     {
-        $storeIds = is_array($value) ? $value : [$value];
+        $websiteIds = is_array($value) ? $value : [$value];
 
-        return in_array(0, $storeIds) && count($storeIds) === 1
-            ? $this->getAllStoreViewsLabel()
-            : implode(', ', map(function ($storeId): string {
-                return $this->systemStore->getStoreNamePath($storeId);
-            }, $storeIds));
+        return in_array(0, $websiteIds) && count($websiteIds) === 1
+            ? $this->getAllWebsitesLabel()
+            : implode(', ', map(function ($websiteId): string {
+                return $this->systemStore->getWebsiteName($websiteId);
+            }, $websiteIds));
     }
 
     public function toHtmlRecursive($value, $maxRecursionDepth = self::UNLIMITED_RECURSION): ?string
     {
-        $storeIds = is_array($value) ? $value : [$value];
-
-        return in_array(0, $storeIds) && count($storeIds) === 1
-            ? $this->getAllStoreViewsLabel()
-            : $this->formatHierarchy($this->systemStore->getStoresStructure(/* isAll: */ false, $storeIds));
+        return $this->toString($value);
     }
 
-    private function formatHierarchy(array $level, int $depth = 0): string
+    private function getAllWebsitesLabel(): string
     {
-        return implode('<br/>', map(function (array $child) use ($depth): string {
-            $next = '<br/>' . $this->formatHierarchy($child['children'] ?? [], $depth + 1);
-            return $this->indentLabel(3 * $depth, $child['label']) . $next;
-        }, $level));
-    }
-
-    private function indentLabel(int $n, string $label): string
-    {
-        return str_repeat('&nbsp;', $n) . $this->escaper->escapeHtml($label);
-    }
-
-    private function getAllStoreViewsLabel(): string
-    {
-        return (string) __('All Store Views');
+        return (string) __('All Websites');
     }
 }
