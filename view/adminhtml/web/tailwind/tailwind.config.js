41203877ac25f1696a09da35bd854bc9671a148f
Vinai Kopp <vinai@netzarbeiter.com>
2021-07-01 16:05:18 +0200

    Make form styling opt-in
    Use forms plugin 0.3.3 to allow this.
    
    This should make the adminhtml styles more compatible with the standard
    Magento forms
    
    See https://github.com/tailwindlabs/tailwindcss-forms#using-classes-instead-of-element-selectors

--- view/adminhtml/web/tailwind/tailwind.config.js
+++ view/adminhtml/web/tailwind/tailwind.config.js
@@ -88,5 +88,7 @@ module.exports = {
     variants: {
         extend: {},
     },
-    plugins: [require("@tailwindcss/forms")],
+    plugins: [
+        require("@tailwindcss/forms")({strategy: 'class'})
+    ],
 };
